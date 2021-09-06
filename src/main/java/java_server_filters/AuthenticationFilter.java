111862ff6b16a59b8c6369c87a256e4a76c57838
Brunilda Briskaj <>
2021-09-06 21:18:58 +0200

    The Filter design pattern

---
+++ src/main/java/java_server_filters/AuthenticationFilter.java
@@ -0,0 +1,49 @@
+package java_server_filters;
+
+import eu.bitwalker.useragentutils.UserAgent;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebFilter(filterName = "authentication")
+public class AuthenticationFilter implements Filter {
+
+    private ServletContext context;
+
+    public void init(FilterConfig fConfig) throws ServletException {
+        this.context = fConfig.getServletContext();
+        this.context.log("AuthenticationFilter initialized");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        UserAgent userAgent = UserAgent.parseUserAgentString(httpServletRequest.getHeader("User-Agent"));
+        if (userAgent.getBrowser().getName().equals("Chrome 9")) {
+            this.context.log("We are in Chrome");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>You are not authorized to enter</font>");
+        } else {
+            chain.doFilter(request, response);
+        }
+
+        // or you can add different logic here: allow access to the servlet if the user is admin
+
+       /** if(httpServletRequest.getSession().getAttribute("USER_TYPE").equals("ADMIN")) {
+            // allow to get access to the servlet
+            chain.doFilter(httpServletRequest, response);
+        } else {
+            // redirect to a specific location
+            ((HttpServletResponse) response).sendRedirect("/admin-login.html");
+        }*/
+    }
+
+    public void destroy() {
+        //we can close resources here
+    }
+}
---
+++ src/main/java/java_server_filters/RequestHandler.java
@@ -0,0 +1,25 @@
+package java_server_filters;
+
+import javax.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "AdminRequestHandler")
+public class RequestHandler extends HttpServlet {
+
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<H2>Welcome User</H2>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
