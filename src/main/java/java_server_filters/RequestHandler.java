6b983944f790035d7dfea980dd1a0a6f8d70b430
Brunilda Briskaj <>
2021-09-06 21:20:49 +0200

    The Filter design pattern

--- src/main/java/java_server_filters/RequestHandler.java
+++ src/main/java/java_server_filters/RequestHandler.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "AdminRequestHandler")
+@WebServlet(name = "RequestHandler")
 public class RequestHandler extends HttpServlet {
 
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse response) throws IOException {
